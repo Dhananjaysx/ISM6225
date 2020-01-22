81a89f240d1e5c0d1cad090ab0f2977d7933f59e
dhana <dhananjays@mail.usf.edu>
2020-01-21 21:01:15 -0500

    commit changes

--- GitHubDemo2/Program.cs
+++ GitHubDemo2/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 
-namespace GitHubDemo2
+namespace week2_exercise
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("This is my change");
-        }
+    {
+        int age;
+        int height;
+        age = 46;
+        height = 61;
+        //Max Heart reate is equal to 220 -age 
+        Console.WriteLine("I am "+ age +" Years old,"+ "My max heart rate is  " +  (220 -age) + " I am  " + height + "  inches tall");
+                  }
     }
 }
